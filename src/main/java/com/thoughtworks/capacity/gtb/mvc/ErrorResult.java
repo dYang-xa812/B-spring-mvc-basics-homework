a33d7092c3e8fd29fc476d7eb6310454fd21fa75
D Yang <moxuan812@foxmail.com>
2021-02-27 12:49:43 +0800

    [yang di] fix: optimization

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/ErrorResult.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/ErrorResult.java
@@ -3,10 +3,12 @@ package com.thoughtworks.capacity.gtb.mvc;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ErrorResult {
+    private HttpStatus code;
     private String message;
 }
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.thoughtworks.capacity.gtb.mvc;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,31 +9,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
-import java.util.Set;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     @ResponseBody
-    public GlobalError handle(UserNotFoundException ex) {
-        return new GlobalError(HttpStatus.NOT_FOUND, ex.getMessage());
+    public ErrorResult handle(UserNotFoundException ex) {
+        return new ErrorResult(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
-    public GlobalError handle(MethodArgumentNotValidException ex) {
-        String message = ex.getBindingResult().getFieldError().getDefaultMessage();
-        return new GlobalError(HttpStatus.BAD_REQUEST,message);
+    public ErrorResult handle(MethodArgumentNotValidException ex) {
+        FieldError error = ex.getBindingResult().getFieldError();
+        String message = ex.getMessage();
+        if (error != null)
+        {
+            message = error.getDefaultMessage();
+        }
+        return new ErrorResult(HttpStatus.BAD_REQUEST,message);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseBody
-    public GlobalError handle(ConstraintViolationException ex) {
+    public ErrorResult handle(ConstraintViolationException ex) {
         String message = "";
         for (ConstraintViolation<?> constraint : ex.getConstraintViolations()) {
             message = constraint.getMessage();
             break;
         }
-        return new GlobalError(HttpStatus.BAD_REQUEST,message);
+        return new ErrorResult(HttpStatus.BAD_REQUEST,message);
     }
 }
